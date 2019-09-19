7ce14457861bc671baca434560e02f9856c98f00
xavier <xavier.kress@gmail.com>
2019-09-19 16:47:14 +0200

    Bean Lifecycle demo

---
+++ src/main/java/com/demospring/xavierkress/springbeanlifecycle/SpringBeanLifecycleApplication.java
@@ -0,0 +1,13 @@
+package com.demospring.xavierkress.springbeanlifecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeanLifecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeanLifecycleApplication.class, args);
+    }
+
+}
